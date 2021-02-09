cdd861c8a23b789e02ce04c326793408a4dd6323
Jonas-VanHaeken <71443869+Jonas-VanHaeken@users.noreply.github.com>
2021-02-09 12:25:53 +0100

    Finished

--- 01-basic-python/07-dictionaries/01-frequencies/student.py
+++ 01-basic-python/07-dictionaries/01-frequencies/student.py
@@ -1,1 +1,9 @@
 # Write your code here
+def frequencies(xs):
+    result = {}
+    for item in xs:
+        if item not in result:
+            result[item] = 1
+        else:
+            result[item] += 1
+    return result
--- 01-basic-python/07-dictionaries/02-from-lists/student.py
+++ 01-basic-python/07-dictionaries/02-from-lists/student.py
@@ -1,1 +1,3 @@
 # Write your code here
+def from_lists(xs, ys):
+    return dict(zip(xs, ys))
